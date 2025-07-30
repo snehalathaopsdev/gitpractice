c11eb6d0e6161aa51a2f8125a7628f12e38d83da
snehalathaopsdev <ksnehalr@gmail.com>
2025-07-30 11:16:11 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World! welcome to java world");
+        System.out.println("Hello, World! welcome to java world the java");
     }
 }
 
